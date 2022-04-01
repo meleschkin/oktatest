e3a885e37fbe198bdf3ca554636a8f3c26c1882d
Andrei Meleschkin <meleschkin@gmail.com>
2022-04-01 08:48:46 +0200

    Initial commit

---
+++ src/main/java/org/meleschkin/okta/OktaClient.java
@@ -0,0 +1,27 @@
+package org.meleschkin.okta;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.DefaultConfiguration;
+import org.pac4j.oidc.client.OidcClient;
+import org.pac4j.oidc.config.OidcConfiguration;
+
+@Log4j2
+public class OktaClient {
+
+    public static void main(String[] args) {
+        Configurator.initialize(new DefaultConfiguration());
+        Configurator.setRootLevel(Level.INFO);
+        try {
+            OidcConfiguration config = new OidcConfiguration();
+            config.setClientId("0oa3vm5l1uVe8mwER0x7");
+            config.setSecret("dZKkDrWvo4SNgVzcao2Irhso2XK_RMoEOpB4AwUk");
+            config.setDiscoveryURI("https://hdi-partner.oktapreview.com");
+            OidcClient oidcClient = new OidcClient(config);
+            log.info(oidcClient.toString());
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+}
