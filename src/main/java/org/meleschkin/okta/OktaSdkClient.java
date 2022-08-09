4b4a9e6a217477b5cadc48c8c6a9957ca0c4364a
Andrei Meleschkin <meleschkin@gmail.com>
2022-08-09 12:20:06 +0200

    OKTA SDK

--- src/main/java/org/meleschkin/okta/OktaSdkClient.java
+++ src/main/java/org/meleschkin/okta/OktaSdkClient.java
@@ -4,6 +4,7 @@ import com.okta.authn.sdk.AuthenticationStateHandler;
 import com.okta.authn.sdk.client.AuthenticationClient;
 import com.okta.authn.sdk.client.AuthenticationClients;
 import com.okta.authn.sdk.resource.AuthenticationResponse;
+import com.okta.authn.sdk.resource.User;
 import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.config.Configurator;
@@ -93,12 +94,21 @@ public class OktaSdkClient {
             AuthenticationResponse authenticationResponse = client.authenticate(
                     "117133_vp", "Okta4HUSKi!!".toCharArray(), null, ignoringStateHandler);
             log.info(authenticationResponse.toString());
+            log.info("Status: " + authenticationResponse.getStatusString());
             String sessionToken = authenticationResponse.getSessionToken();
             log.info("SessionToken: " + sessionToken);
             Date epiresAt = authenticationResponse.getExpiresAt();
             String pattern = "dd.MM.yyyy HH:mm:ss.SSS";
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
             log.info("ExpiresAt: " + simpleDateFormat.format(epiresAt));
+            User user = authenticationResponse.getUser();
+            log.info(user.toString());
+            String login = user.getLogin();
+            log.info("Login: " + login);
+            String firstName = user.getFirstName();
+            log.info("FirstName: " + firstName);
+            String lastName = user.getLastName();
+            log.info("LastName: " + lastName);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
